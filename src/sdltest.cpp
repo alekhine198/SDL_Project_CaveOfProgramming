24d355158f793692db17105be2f73b5793244496
Angel Alekhine <angel.alekhine@yahoo.com>
2019-12-08 22:18:24 -0600

    readding sdltest.cpp after saving it

--- src/sdltest.cpp
+++ src/sdltest.cpp
@@ -22,27 +22,42 @@ int main(int argc, char* argv[]) {
 	SDL_WINDOWPOS_CENTERED,
 	SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-	if(window == NULL){
+	if (window == NULL) {
 		SDL_Quit();
 		printf("cannot open window %s\n", SDL_GetError());
 		return 2;
 	}
 
-	SDL_Renderer * renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_PRESENTVSYNC);
+	SDL_Renderer * renderer = SDL_CreateRenderer(window, -1,
+			SDL_RENDERER_PRESENTVSYNC);
 
-	SDL_Texture * texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_STATIC,SCREEN_WIDTH, SCREEN_HEIGHT);
+	SDL_Texture * texture = SDL_CreateTexture(renderer,
+			SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH,
+			SCREEN_HEIGHT);
 
-	if(renderer == NULL) printf("%s\n", SDL_GetError());
-	if(texture == NULL) printf("%s\n",SDL_GetError());
+	if (renderer == NULL) {
+		printf("%s\n", SDL_GetError());
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 2;
+	}
+
+	if (texture == NULL) {
+		printf("%s\n", SDL_GetError());
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 3;
+	}
 
 	SDL_Event event;
 
 	bool quit = false;
 
-	while(!quit){
+	while (!quit) {
 
-		while(SDL_PollEvent(&event)){
-			if(event.type == SDL_QUIT){
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
 				quit = true;
 			}
 		}
